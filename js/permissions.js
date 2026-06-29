37bc7afe07b76664a735cb785c173eff8ab7d773
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-06-28 22:09:20 -0600

    Create permissions.js

---
+++ js/permissions.js
@@ -0,0 +1,92 @@
+/*==========================================================
+  permissions.js
+  PART 1 OF N
+==========================================================*/
+
+"use strict";
+
+/*==========================================================
+  F.R.I.E.N.D.
+  Permissions Engine
+  Version 1.0
+==========================================================*/
+
+const FRIENDPermissions = (() => {
+
+const ROLES = {
+
+    EXECUTIVE: "EXECUTIVE",
+    STORE_LEADER: "STORE_LEADER",
+    DEPARTMENT_LEADER: "DEPARTMENT_LEADER",
+    ASSOCIATE: "ASSOCIATE"
+
+};
+
+const PERMISSIONS = {
+
+    EXECUTIVE: [
+        "VIEW_ALL",
+        "EDIT_ALL",
+        "VIEW_ANALYTICS",
+        "VIEW_FINANCIALS",
+        "MANAGE_USERS"
+    ],
+
+    STORE_LEADER: [
+        "VIEW_STORE",
+        "EDIT_STORE",
+        "VIEW_ANALYTICS",
+        "MANAGE_DEPARTMENTS"
+    ],
+
+    DEPARTMENT_LEADER: [
+        "VIEW_DEPARTMENT",
+        "EDIT_DEPARTMENT",
+        "VIEW_MISSIONS"
+    ],
+
+    ASSOCIATE: [
+        "VIEW_ASSIGNED_MISSIONS",
+        "COMPLETE_TASKS"
+    ]
+
+};
+
+let currentRole = ROLES.ASSOCIATE;
+
+function setRole(role){
+
+    if(!ROLES[role]) return false;
+
+    currentRole = role;
+
+    return currentRole;
+
+}
+
+function getRole(){
+
+    return currentRole;
+
+}
+
+return {
+
+    ROLES,
+    setRole,
+    getRole
+
+};
+
+})();
+
+/*==========================================================
+  Global Access
+==========================================================*/
+
+window.FRIENDPermissions =
+    FRIENDPermissions;
+
+/*==========================================================
+  End permissions.js
+==========================================================*/
