11dfb57ffbfc38b3a18f1b78d532317a2390f365
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-06-28 19:17:20 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
-
+const FRIEND_CONFIG = {
+  appName: "F.R.I.E.N.D.",
+  fullName: "Full Retail Integration Engaged Network Dynamics",
+  version: "1.0.0",
+  store: {
+    number: "129",
+    district: "2",
+    division: "620"
+  },
+  roles: {
+    associate: "Associate",
+    departmentLeader: "Department Leader",
+    storeLeader: "Store Leader",
+    executive: "Executive"
+  }
+};
