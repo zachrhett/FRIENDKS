a0d9a29ba26448c39d8345751451e48b2c97cfe2
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-06-28 19:19:07 -0600

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,23 @@
+const FRIEND_NAVIGATION = {
+  routes: {
+    home: "Home",
+    associate: "Associate Mode",
+    departmentLeader: "Department Leader",
+    storeLeader: "Store Leader",
+    executive: "Executive Dashboard",
+    maximo: "Maximo",
+    alerts: "Alerts",
+    scorecard: "Store Scorecard",
+    composite: "Composite Scorecard"
+  },
 
+  goTo(routeName) {
+    if (!this.routes[routeName]) {
+      console.warn("Unknown route:", routeName);
+      return;
+    }
+
+    FRIEND_STATE.currentModule = routeName;
+    FRIEND_APP.loadView(routeName);
+  }
+};
