7e388c199fd7d6378fbcbfef0e1462b9a809c62d
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-06-28 19:19:43 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+console.log("F.R.I.E.N.D. main.js loaded");
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Main connection ready");
+
+  if (typeof FRIEND_APP !== "undefined") {
+    FRIEND_APP.init();
+  }
+});
