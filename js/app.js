c1aef6390081a5d4c9a6ebf9e10d5db1a7a705e0
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-06-28 19:17:49 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+const FRIEND_APP = {
+  currentView: "home",
 
+  init() {
+    console.log("F.R.I.E.N.D. app initialized");
+    this.loadView("home");
+  },
+
+  loadView(viewName) {
+    this.currentView = viewName;
+    console.log("Loading view:", viewName);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  FRIEND_APP.init();
+});
